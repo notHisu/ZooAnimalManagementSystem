cb9656f5d9d6e42c42da677e59a62fd557b0dd17
Le Trung Hoa Hieu <nothisu.dev@gmail.com>
2025-02-18 14:52:40 +0700

    feat: add LINQ method to filter and retrieve animals

--- ZooAnimalManagementSystem/Program.cs
+++ ZooAnimalManagementSystem/Program.cs
@@ -8,11 +8,43 @@
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            Zoo zoo = new Zoo();
+
+            zoo.AddAnimal(new Lion("Ura", 5));
+            zoo.AddAnimal(new Lion("Mufasa", 7));
+            zoo.AddAnimal(new Lion("Simba", 3));
+
+            zoo.AddAnimal(new Elephant("Eren", 10));
+            zoo.AddAnimal(new Elephant("Dumbo", 2));
+            zoo.AddAnimal(new Elephant("Horton", 4));
+
+            zoo.AddAnimal(new Monkey("Kong", 3));
+            zoo.AddAnimal(new Monkey("Rafiki", 6));
+
+            zoo.ShowAnimals();
+
+            var bySpecies = zoo.GetAnimalsByCriteria(animal => animal.Species == "Lion");
+            
+            foreach (var animal in bySpecies)
+            {
+                animal.ShowInfo();
+                Console.WriteLine();
+            }
+
+            var byAge = zoo.GetAnimalsByCriteria(animal => animal.Age > 5);
+
+            foreach (var animal in byAge)
+            {
+                animal.ShowInfo();
+                Console.WriteLine();
+            }
+
+            /*
             Lion lion = new Lion("Ura", 5);
             Elephant elephant = new Elephant("Eren", 10);
             Monkey monkey = new Monkey("Kong", 3);
             
-            //Console.WriteLine("Lion's name is " + lion.Name);
+            Console.WriteLine("Lion's name is " + lion.Name);
             lion.ShowInfo();
             lion.MakeSound();
 
@@ -21,6 +53,7 @@
 
             monkey.ShowInfo();
             monkey.MakeSound();
+            */
 
 
         }
---
+++ ZooAnimalManagementSystem/Zoo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooAnimalManagementSystem
+{
+    public class Zoo
+    {
+        private List<Animal> animals;
+
+        public List<Animal> Animals
+        {
+            get { return animals; }
+            set { animals = value; }
+        }
+
+        public Zoo()
+        {
+            animals = new List<Animal>();
+        }
+        public void AddAnimal(Animal animal)
+        {
+            animals.Add(animal);
+        }
+        public void ShowAnimals()
+        {
+            foreach (Animal animal in animals)
+            {
+                animal.ShowInfo();
+                animal.MakeSound();
+            }
+        }
+
+        public IEnumerable<Animal> GetAnimalsByCriteria(Func<Animal, bool> criteria)
+        {
+            if (criteria == null)
+            {
+                throw new ArgumentNullException("criteria");
+            }
+            return animals.Where(criteria);
+        }
+
+    }
+}
