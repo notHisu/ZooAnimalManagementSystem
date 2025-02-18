1e69c9bdb0e5b854d0a1235119084005b1d0d853
Le Trung Hoa Hieu <nothisu.dev@gmail.com>
2025-02-18 09:59:40 +0700

    Update Program.cs

--- ZooAnimalManagementSystem/Program.cs
+++ ZooAnimalManagementSystem/Program.cs
@@ -9,15 +9,16 @@
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             Lion lion = new Lion("Ura", 5);
+            Elephant elephant = new Elephant("Eren", 10);
+            Monkey monkey = new Monkey("Kong", 3);
+            
             //Console.WriteLine("Lion's name is " + lion.Name);
             lion.ShowInfo();
             lion.MakeSound();
 
-            Elephant elephant = new Elephant("Eren", 10);
             elephant.ShowInfo();
             elephant.MakeSound();
 
-            Monkey monkey = new Monkey("Kong", 3);
             monkey.ShowInfo();
             monkey.MakeSound();
 
